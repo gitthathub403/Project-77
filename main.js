2cdd8a9f64356186a88f7b6dbe405d393033d65e
gitthathub403 <117359970+gitthathub403@users.noreply.github.com>
2023-04-30 19:44:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var f_name = "Ranbir";
+var l_name = "Kapoor";
+var full_name = f_name.concat(l_name);
+console.log(full_name);
+
+var a= 10;
+var b=5;
+var add=a+b;
+console.log(add);
+
+var sub=a-b;
+console.log(sub);
+
+var mul=a*b;
+console.log(mul);
+
+var div=a/b;
+console.log(div);
